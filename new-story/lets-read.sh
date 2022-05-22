2011a13541fc764dfa6d7c227730a5ca234b0a9c
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-05-22 14:29:23 +0000

    use tesseract to generate pdf

--- new-story/lets-read.sh
+++ new-story/lets-read.sh
@@ -2,15 +2,23 @@
 # Usage: lets-read.sh <youtube url> <output file without extension> <optional scene change threshold>
 
 if [ ! -e ${2}.mp4 ]; then
-    youtube-dl $1 -o $2
+    youtube-dl --get-filename $1 -o $2
     ffmpeg -i ${2}.mkv -codec copy ${2}.mp4
     rm ${2}.mkv
 fi
 
-threshold=${3:-1}
+threshold=${3:-1} # default 1 fps
 
-ffmpeg -i ${2}.mp4 -vf "fps=${threshold}" -vsync vfr ${2}-frame-%6d.jpg
+if [ ! -d ${2} ]; then
+    mkdir ${2}
+fi
 
-convert ${2}-frame-*.jpg ${2}.pdf
+ffmpeg -i ${2}.mp4 -vf "fps=${threshold}" -vsync vfr ${2}/frame-%6d.jpg
 
-# rm ${2}-frame-*.jpg
+rm ${2}/*.txt
+
+haxelib dev tesseract-all tesseract-all
+haxelib run tesseract-all ${2}
+convert ${2}/frame-*.jpg ${2}.pdf
+
+# rm ${2}/frame-*.jpg
---
+++ new-story/tesseract-all/src/tesseract_all/Main.hx
@@ -0,0 +1,11 @@
+package tesseract_all;
+
+import kiss.Kiss;
+import kiss.Prelude;
+import sys.FileSystem;
+import sys.io.File;
+using hx.strings.Strings;
+using haxe.io.Path;
+
+@:build(kiss.Kiss.build())
+class Main {}
