e37175c33d0c3b4182dc97635870c724c2cbfa5d
Noah Nelson <nelson.noah96@gmail.com>
2022-06-11 22:39:43 +0000

    Add encounter to debug menu

--- src/debug_menu.c
+++ src/debug_menu.c
@@ -1,15 +1,19 @@
 #if DEBUG
 
 #include "global.h"
+#include "battle_setup.h"
 #include "list_menu.h"
 #include "main.h"
 #include "map_name_popup.h"
 #include "menu.h"
 #include "script.h"
+#include "script_pokemon_util.h"
 #include "sound.h"
 #include "strings.h"
 #include "task.h"
+#include "constants/items.h"
 #include "constants/songs.h"
+#include "constants/species.h"
 
 #define DEBUG_MAIN_MENU_HEIGHT 7
 #define DEBUG_MAIN_MENU_WIDTH 11
@@ -17,21 +21,26 @@
 void Debug_ShowMainMenu(void);
 static void Debug_DestroyMainMenu(u8);
 static void DebugAction_Cancel(u8);
+static void DebugAction_Encounter(u8);
 static void DebugTask_HandleMainMenuInput(u8);
 
 enum {
+    DEBUG_MENU_ITEM_ENCOUNTER,
     DEBUG_MENU_ITEM_CANCEL,
 };
 
+static const u8 gDebugText_Encounter[] = _("Encounter");
 static const u8 gDebugText_Cancel[] = _("Cancel");
 
 static const struct ListMenuItem sDebugMenuItems[] =
 {
+    [DEBUG_MENU_ITEM_ENCOUNTER] = {gDebugText_Encounter, DEBUG_MENU_ITEM_ENCOUNTER},
     [DEBUG_MENU_ITEM_CANCEL] = {gDebugText_Cancel, DEBUG_MENU_ITEM_CANCEL}
 };
 
 static void (*const sDebugMenuActions[])(u8) =
 {
+    [DEBUG_MENU_ITEM_ENCOUNTER] = DebugAction_Encounter,
     [DEBUG_MENU_ITEM_CANCEL] = DebugAction_Cancel
 };
 
@@ -123,6 +132,13 @@ static void DebugTask_HandleMainMenuInput(u8 taskId)
 static void DebugAction_Cancel(u8 taskId)
 {
     Debug_DestroyMainMenu(taskId);
+}
+
+static void DebugAction_Encounter(u8 taskId)
+{
+    Debug_DestroyMainMenu(taskId);
+    CreateScriptedWildMon(SPECIES_KECLEON, 35, ITEM_NONE);
+    BattleSetup_StartScriptedWildBattle();
 }
 
 #endif
